24c99a44e629e9892745ef3517757287d0362e6e
hywax <a.hywax@gmail.com>
2023-12-21 23:30:16 +0500

    chore: configure eslint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  typescript: true,
+  ignores: [
+    '.output',
+    '.nitro',
+    '.output',
+    'dist',
+    'node_modules',
+  ],
+  rules: {
+    'style/brace-style': ['error', '1tbs'],
+    'style/arrow-parens': ['error', 'always'],
+    'curly': ['error', 'all'],
+    'antfu/consistent-list-newline': 'off',
+  },
+})
