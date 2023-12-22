72ee60dd2adf15936f52d56ffbd32cb10de8aef0
hywax <a.hywax@gmail.com>
2023-12-22 20:39:49 +0500

    feat: browser initial params

--- utils/browser.ts
+++ utils/browser.ts
@@ -3,10 +3,27 @@ import { launch } from 'puppeteer'
 
 let browser: Browser | null = null
 
-export async function useBrowser() {
+export async function useBrowser(): Promise<Browser> {
+  const { browserViewportWidth, browserViewportHeight, browserChromePath } = useRuntimeConfig()
+
   if (!browser) {
     browser = await launch({
       headless: 'new',
+
+      ...(browserChromePath)
+        ? {
+            executablePath: browserChromePath,
+          }
+        : {},
+
+      ...(browserViewportWidth && browserViewportHeight)
+        ? {
+            defaultViewport: {
+              height: browserViewportHeight,
+              width: browserViewportWidth,
+            },
+          }
+        : {},
     })
   }
 
